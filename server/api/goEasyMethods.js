2a84ba80b6c655af0fa7abd17356e83a6f32b0e4
gssmages <mageshbabu.s@flex.com>
2018-02-15 12:32:23 +0530

    mytrips

--- server/api/goEasyMethods.js
+++ server/api/goEasyMethods.js
@@ -24,3 +24,24 @@ exports.getDashboard = function (page,params) {
     xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xmlhttp.send();
 }
+exports.getMytrips = function (page,params) {
+
+    var xmlhttp = new XMLHttpRequest();
+    
+    var url = "http://gssnte811.asia.ad.flextronics.com:4042/api/MyTripsApi/GetMyTripsDetails/?employeeID=941364&todaydate=12-01-2017";
+    var reqResponse = [];
+    
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+
+            // page.data(function(data) {
+            //     data.dashboard_list = response;
+            // })
+            //     .screen("dashboard");
+        }
+    };
+    xmlhttp.open("GET", url, false);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.send();
+}
