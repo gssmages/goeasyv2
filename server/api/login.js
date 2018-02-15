f0ec1bbb0353a72f36ea71d4cebc0d36284315a7
gssmages <32328556+gssmages@users.noreply.github.com>
2018-02-15 17:27:03 +0530

    login server

--- server/api/login.js
+++ server/api/login.js
@@ -7,19 +7,20 @@ var btoa = require('btoa');
 
 exports.login = function (page,params) {
     var xmlhttp = new XMLHttpRequest();
-    var url = "http://sacnte245.americas.ad.flextronics.com:9094/Authentication";
-    var reqResponse = [];
-    var enCredential ={'UserName':btoa(params.UserName),'PassWord':btoa(params.PassWord)}
+    // var url = "http://sacnte245.americas.ad.flextronics.com:9094/Authentication";
+    // var reqResponse = [];
+    // var enCredential ={'UserName':btoa(params.UserName),'PassWord':btoa(params.PassWord)}
+    var url = "http://gssnte811.asia.ad.flextronics.com:4042/api/DashBoardApi/GetDashboardDetails/?todaysdate=12-01-2017&location=1&employeeID=941364"
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(this.responseText);
             // console.log("response-->"+response.IsSuccess);
             // console.log("IsDefaultCompany-->"+response.IsDefaultCompany);
             
-            // page.data(function(data) {
-            //     data.sitelist = reqResponse;
-            // })
-            //     .screen("sitepage");
+            page.data(function(data) {
+                data.home = reqResponse;
+            })
+                .screen("home");
         }
     };
     xmlhttp.open("POST", url, false);
