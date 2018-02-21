d2b1f348db8b4a608aebdbf7b44806fc18eb35d6
selvabharathi <selvabharathi.p@flex.com>
2018-02-21 03:23:14 -0800

    [Studio] Modified screen "adhocrequest"

--- client/controllers/app/adhocrequest.js
+++ client/controllers/app/adhocrequest.js
@@ -4,32 +4,32 @@ function app_adhocrequest($scope, app) {
     app.init($scope, function () {
         console.log('data objects ', $scope.data);
         $scope.typeChange = function () {
-            console.log('---Request Type---' + JSON.stringify($scope.data.RequestType.selected.label));
-            var selectedReqTyp = $scope.data.RequestType.selected.value;
-            $scope.submitApprovals();
-            switch (selectedReqTyp) {
-            case '2':
-                $scope.dateField = true;
-                $scope.fromDateField = false;
-                $scope.toDateField = false;
-                break;
-            case '3':
-                $scope.dateField = true;
-                $scope.fromDateField = false;
-                $scope.toDateField = false;
-                break;
-            case '4':
-                $scope.dateField = false;
-                $scope.fromDateField = true;
-                $scope.toDateField = true;
-                break;
-            default:
-                console.log('in default--' + selectedReqTyp);
-                $scope.dateField = false;
-                $scope.fromDateField = false;
-                $scope.toDateField = false;
-                break;
-            }
+            // console.log('---Request Type---' + JSON.stringify($scope.data.RequestType.selected.label));
+            // var selectedReqTyp = $scope.data.RequestType.selected.value;
+            // $scope.submitApprovals();
+            // switch (selectedReqTyp) {
+            // case '2':
+            //     $scope.dateField = true;
+            //     $scope.fromDateField = false;
+            //     $scope.toDateField = false;
+            //     break;
+            // case '3':
+            //     $scope.dateField = true;
+            //     $scope.fromDateField = false;
+            //     $scope.toDateField = false;
+            //     break;
+            // case '4':
+            //     $scope.dateField = false;
+            //     $scope.fromDateField = true;
+            //     $scope.toDateField = true;
+            //     break;
+            // default:
+            //     console.log('in default--' + selectedReqTyp);
+            //     $scope.dateField = false;
+            //     $scope.fromDateField = false;
+            //     $scope.toDateField = false;
+            //     break;
+            // }
         };
         $scope.submitApprovals = function () {
             app.call('myapprovals.sendRequest', $scope.data);
