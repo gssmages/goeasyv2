3dcfcca80e49fcd85b63991612394534132b06d4
Magesh Babu S <mageshbabu.s@flex.com>
2018-02-21 23:38:17 -0800

    [Studio] Add property to screen model "tripCancellation", Modified screen "tripCancellation"

--- client/controllers/app/tripcancellation.js
+++ client/controllers/app/tripcancellation.js
@@ -2,7 +2,7 @@ angular.module('app').controller('app_tripcancellation', app_tripcancellation);
 function app_tripcancellation($scope, app) {
     'use strict';
     app.init($scope,function(){
-        $scope.Requestfor=[
+        $scope.requestfor=[
             { "RequestForID":"1","RequestForName":"Pickup and Drop"},
             { "RequestForID":"2","RequestForName":"Pickup"},
             { "RequestForID":"3","RequestForName":"Drop"}
