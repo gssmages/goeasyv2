cd844280b738a170b9c748e3140a7f41213bdc47
Magesh Babu S <mageshbabu.s@flex.com>
2018-02-22 03:12:56 -0800

    [Studio] Modified screen "myapprovals"

--- client/controllers/app/myapprovals.js
+++ client/controllers/app/myapprovals.js
@@ -1,4 +1,5 @@
-angular.module('app').controller('app_myapprovals',app_myapprovals).directive("approvallist",function($ionicGesture,$rootScope){
+angular.module('app').controller('app_myapprovals',app_myapprovals)
+.directive("approvallist",function($ionicGesture,$rootScope){
     return {
         scope: {},
         restrict: "A",
@@ -10,50 +11,19 @@ angular.module('app').controller('app_myapprovals',app_myapprovals).directive("a
 function app_myapprovals($scope, app){
     'use strict';
     app.init($scope);
-    $scope.$on('$ionicView.enter', function(){
-        var index1 = 0;
-        angular.forEach($scope.data.listItems, function (value) {
-            if($scope.data.listItems[index1].isselect=== true)
-            {
-                $scope.data.listItems[index1].isselect=false;
-            }
-            index1=index1+1;
-        });
-    });
     $scope.selectbox = function(idvalue,elem) {
-        var index = 0;
-        angular.forEach($scope.data.listItems, function (value) {
-            if(index===idvalue)
-            {
-                if($scope.data.listItems[index].isselect=== true)
-                {
-                    $scope.data.listItems[index].isselect=false;
-                }
-                else
-                {
-                    $scope.data.listItems[index].isselect=true;
-                }
+        $(".card").each(function($index){
+            if($(this).find(".approvallist").not("selected"))
+            {	
+                $(this).find(".approvallist").addClass("selected").css('border-top','5px solid #000');
             }
-            else
-            {
-                if($scope.data.listItems[index].isselect=== true)
-                {
-                    $scope.data.listItems[index].isselect=true;
-                }
-                else
-                {
-                    $scope.data.listItems[index].isselect=false;
-                }
-            }
-            index=index+1;
-        }); 
+        });
     };    
     $scope.selectionall = function() {
         $(".card").each(function($index){
             if($(this).find(".approvallist").not("selected"))
             {	
                 $(this).find(".approvallist").addClass("selected").css('border-top','5px solid #000');
-                $scope.data.listItems[$index].isselect=true;
             }
         });
     };
@@ -62,7 +32,6 @@ function app_myapprovals($scope, app){
             if($(this).find(".approvallist").hasClass("selected"))
             {
                 $(this).find(".approvallist").removeClass("selected").css('border-top','5px solid #ddd');
-                $scope.data.listItems[$index].isselect=false;
             }
         });
     };
