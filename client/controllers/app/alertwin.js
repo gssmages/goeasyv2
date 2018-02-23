3ac02b5ecb7cfe2c4b028a910ed1a63761c90983
Magesh Babu S <mageshbabu.s@flex.com>
2018-02-22 23:38:51 -0800

    [Studio] Modified screen "alertwin"

--- client/controllers/app/alertwin.js
+++ client/controllers/app/alertwin.js
@@ -1,5 +1,8 @@
 angular.module('app').controller('app_alertwin', app_alertwin);
 function app_alertwin($scope, app) {
     'use strict';
-    app.init($scope);
+    app.init($scope,function(data){
+       console.log(data.approvalconfirm);
+     //$scope.cancellist=false;
+    });
 }
