b5be02339a7723eb46f869ca861e67770d3b95df
Magesh Babu S <mageshbabu.s@flex.com>
2018-02-23 09:33:16 -0800

    [Studio] Modified screen "alertwin"

--- client/controllers/app/alertwin.js
+++ client/controllers/app/alertwin.js
@@ -2,16 +2,16 @@ angular.module('app').controller('app_alertwin', app_alertwin);
 function app_alertwin($scope, app) {
     'use strict';
     app.init($scope,function(data){
-        
+        $scope.comments;
       
      //$scope.cancellist=false;
     });
-    $scope.comments="";
       $scope.sendrequest=function(){
           var sendtoconfirm = '';
          var cabreqid= JSON.stringify($scope.data.approvalinfo.cabRequestID);
          var statusid=$scope.data.approvalinfo.status;
-         sendtoconfirm={"cabRequestID":cabreqid,"status":statusid,"approver":"880781","remarks":comments}
+         var comment=$scope.comments;
+         sendtoconfirm={"cabRequestID":cabreqid,"status":statusid,"approver":"880781","remarks":comment}
        console.log(sendtoconfirm);
           // app.call("goeasymethods.approvalrequest",sendtoconfirm);
       };
