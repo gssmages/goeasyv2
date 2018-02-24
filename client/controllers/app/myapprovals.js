17c2a709fe4429b0636073bab68e96d8d68b39c7
Magesh Babu S <mageshbabu.s@flex.com>
2018-02-23 21:59:32 -0800

    [Studio] Modified screen "myapprovals"

--- client/controllers/app/myapprovals.js
+++ client/controllers/app/myapprovals.js
@@ -31,11 +31,11 @@ function app_myapprovals($scope, app, $ionicPopup){
      });
      var success=$scope.data.successmsg;
      console.log(success+"thisissuccesmsg");
-      if(success!==undefined)
+      if(success!=="undefined")
          {
           var alertPopup = $ionicPopup.alert({
      title: 'Goeasy message',
-     template: $scope.data.successmsg
+     template: success
    });
 
    alertPopup.then(function(res) {
