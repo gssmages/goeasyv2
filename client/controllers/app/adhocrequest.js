6f0d73181431c8a887004bfba6dd9bf271ff9aa5
selvabharathi <selvabharathi.p@flex.com>
2018-02-26 00:06:10 -0800

    [Studio] Modified screen "adhocrequest"

--- client/controllers/app/adhocrequest.js
+++ client/controllers/app/adhocrequest.js
@@ -10,6 +10,7 @@ function app_adhocrequest($scope, app) {
         $scope.boardingPoint= $scope.data.adhocDataList[0].RelBoardingPointDetails;
         $scope.areaSelect = function () {
             console.log("area selected-->"+ $scope.data.selectedarea);
+            var i=0;
             for(i;i<$scope.data.adhocDataList[0].AreaDetails.length;i++){
                 
             if ($scope.data.adhocDataList[0].AreaDetails[i].AreaName == $scope.data.selectedarea){
