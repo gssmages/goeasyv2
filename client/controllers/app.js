11163897415774d724af72d11dddf59db397f9fe
Magesh Babu S <mageshbabu.s@flex.com>
2018-02-28 02:51:42 -0800

    [Studio] Modified screen "goeasyv2"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -1,4 +1,9 @@
 angular.module('app').controller('app', app);
 function app($scope, app) {
     'use strict';
+     app.init($scope,function(){
+       console.log("getting display name : "+$localStorage.displayName);
+        $scope.displayName = $localStorage.displayName;
+        $scope.businessTitle = $localStorage.businessTitle;
+    });
 }
