40ae479acb07271af20d2691e1128f84369fa887
Magesh Babu S <mageshbabu.s@flex.com>
2018-02-28 03:02:08 -0800

    [Studio] Modified screen "goeasyv2", Modified screen "Home"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -2,8 +2,10 @@ angular.module('app').controller('app', app);
 function app($scope, app,$localStorage) {
     'use strict';
      app.init($scope,function(){
+         $scope.displayName = '';
+        $scope.businessTitle='';
        console.log("getting display name : "+$localStorage.displayName);
-        $scope.displayName = $localStorage.displayName;
-        $scope.businessTitle = $localStorage.businessTitle;
+       // $scope.displayName = $localStorage.displayName;
+        //$scope.businessTitle = $localStorage.businessTitle;
     });
 }
--- client/controllers/app/home.js
+++ client/controllers/app/home.js
@@ -8,7 +8,8 @@ function app_home($scope, app, $localStorage) {
        $localStorage.businessTitle = $scope.data.home.EmployeeDetails.BusinessTitle;
        $localStorage.employeeId = $scope.data.home.EmployeeDetails.EmployeeID;
        
-       
+       $scope.displayName=$scope.data.home.EmployeeDetails.DisplayName;
+        $scope.businessTitle =$scope.data.home.EmployeeDetails.BusinessTitle;
         
        $scope.pickupdropdiv = true;
        $scope.nopickupdiv = false;
