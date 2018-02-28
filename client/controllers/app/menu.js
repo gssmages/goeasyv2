2f257a6c0415fac8eb3245dbb1b8d27ffc20a302
Magesh Babu S <mageshbabu.s@flex.com>
2018-02-28 02:37:59 -0800

    [Studio] Modified screen "menu"

--- client/controllers/app/menu.js
+++ client/controllers/app/menu.js
@@ -2,8 +2,8 @@ angular.module('app').controller('app_menu', app_menu);
 function app_menu($scope,$location,$window, app,$localStorage) {
     'use strict';
     app.init($scope,function(){
-       /* console.log("getting display name : "+$localStorage.displayName);
-        $scope.data.displayName = $localStorage.displayName;
+       console.log("getting display name : "+$localStorage.displayName);
+        /*$scope.data.displayName = $localStorage.displayName;
         $scope.data.businessTitle = $localStorage.businessTitle;*/
     });
 }
