2ce90038e5e5e74f10af05cdeece850d0a5845ba
selvabharathi <selvabharathi.p@flex.com>
2018-03-01 02:12:52 -0800

    [Studio] Modified screen "adhocrequest"

--- client/controllers/app/adhocrequest.js
+++ client/controllers/app/adhocrequest.js
@@ -24,7 +24,7 @@ function app_adhocrequest($scope, app, $ionicPopup) {
             //  if(date.getHours()/12 > 0)
             //     hrs = date.getHours()%12 + "PM"
             //  else hrs = date.getHours()%12 +"AM"
-            console.log("shift selected-->"+item);
+            console.log("shift selected-->"+JSON.stringify(item));
         }
         
         $scope.NeedCheck = function(){
