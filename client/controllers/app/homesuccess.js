874464f108e86909069dc15f752455953af99181
Magesh Babu S <mageshbabu.s@flex.com>
2018-03-01 02:12:39 -0800

    [Studio] Modified screen "homesuccess"

--- client/controllers/app/homesuccess.js
+++ client/controllers/app/homesuccess.js
@@ -2,6 +2,12 @@ angular.module('app').controller('app_homesuccess', app_homesuccess);
 function app_homesuccess($scope, app,$filter,$localStorage) {
     'use strict';
     app.init($scope,function(){
+        $ionicLoading.show({
+      template: 'Loading...',
+      duration: 3000
+    }).then(function(){
+       console.log("The loading indicator is now displayed");
+    });
          var todaysdate=$filter('date')(new Date(), 'MM-dd-yyyy');
           $localStorage.employeeID=$scope.data.home.EmployeeID;
            $localStorage.locationName=$scope.data.home.LocationName;
