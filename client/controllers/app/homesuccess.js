6c38fd326331aed5ac1e8d20ea9cfca50e215a29
Magesh Babu S <mageshbabu.s@flex.com>
2018-03-01 01:29:46 -0800

    [Studio] Modified screen "homesuccess"

--- client/controllers/app/homesuccess.js
+++ client/controllers/app/homesuccess.js
@@ -3,6 +3,10 @@ function app_homesuccess($scope, app,$filter) {
     'use strict';
     app.init($scope,function(){
          var todaysdate=$filter('date')(new Date(), 'MM-dd-yyyy');
+          var employeeID=$scope.data.home.EmployeeID;
+           var LocationName=$scope.data.home.EmployeeID;
+            var employeeID=$scope.data.home.EmployeeID;
+           var todaysdate=$filter('date')(new Date(), 'MM-dd-yyyy');
          var params={"employeeID":$localStorage.employeeId,"todaysdate":todaysdate,"location":$localStorage.location};
             console.log(params);
             app.call('goeasymethods.getDashboard',params);
