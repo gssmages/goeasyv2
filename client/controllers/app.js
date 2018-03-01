62ff91cdcfb236983067a2217631906ee14a47b2
Magesh Babu S <mageshbabu.s@flex.com>
2018-02-28 19:00:42 -0800

    [Studio] Modified screen "menu", Modified screen "goeasyv2"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -13,5 +13,17 @@ function app($scope, app,$localStorage) {
  $scope.displayName = $localStorage.displayName;
  $scope.businessTitle = $localStorage.businessTitle;
 }, 1000);
-
+   
+    
+ $scope.menudata = function(data) {
+          console.log("calling mytrips in menu call");
+        if(data=="mytrips")
+        {
+            var todaysdate=$filter('date')(new Date(), 'MM-dd-yyyy');
+            var params={"employeeID":$localStorage.employeeId,"todaysdate":todaysdate};
+            console.log(params);
+            app.call('goeasymethods.getMytrips',params);
+        }
+       
+    };
 }
