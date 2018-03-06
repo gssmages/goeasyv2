0d65cb9ffc426d4f8696871f34a45629367965d3
gssmages <mageshbabu.s@flex.com>
2018-03-06 18:17:41 +0530

    for test purpose

--- server/api/login.js
+++ server/api/login.js
@@ -8,8 +8,9 @@ var goEasyMethods = require('./goeasymethods.js');
 exports.login = function (page,params) {
     var xmlhttp = new XMLHttpRequest();
    
-    //var url = "http://gssnte811.asia.ad.flextronics.com:4042/api/login/CheckLoginDetailEncryption?userName="+btoa(params.UserName)+"&password ="+btoa(params.PassWord);
-    var url = "http://gssnte811.asia.ad.flextronics.com:4042/api/login/CheckLoginDetailEncryption?userName="+params.username+"&password="+params.password;
+    //var url = "http://gssnte811.asia.ad.flextronics.com:4042/api/login/CheckLoginDetailEncryption?userName="+btoa(params.UserName)+"&password ="+btoa(params.PassWord
+    page.screen("home");
+  /*  var url = "http://gssnte811.asia.ad.flextronics.com:4042/api/login/CheckLoginDetailEncryption?userName="+params.username+"&password="+params.password;
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(this.responseText);
@@ -26,7 +27,7 @@ exports.login = function (page,params) {
             })
                 .screen("login");
         }
-    };
+    };*/
     xmlhttp.open("GET", url, false);
     xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     //xmlhttp.send(JSON.stringify(enCredential));
