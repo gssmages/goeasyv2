eb338922d38fe9201d6140092728af61207dadb1
selvabharathi <selvabharathi.p@flex.com>
2018-03-07 23:55:19 -0800

    [Studio] Publish application, Modified screen "myapprovals"

--- client/controllers/app/myapprovals.js
+++ client/controllers/app/myapprovals.js
@@ -10,7 +10,7 @@ angular.module('app').controller('app_myapprovals',app_myapprovals)
             elem.addClass("selected").css('border-top','5px solid #000');
             $rootScope.startSelect = true; // to enable select box by click
           }, elem);
-		 $ionicGesture.on('tap',function(){
+		$ionicGesture.on('tap',function(){
           	if ($rootScope.startSelect) {
            		if (elem.hasClass('selected')) {
               	elem.removeClass('selected').css('border-top','5px solid #ddd');
