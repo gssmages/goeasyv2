5877e0d65654b0c299746a6efa312871cf75ff0c
Magesh Babu S <mageshbabu.s@flex.com>
2018-03-12 01:14:48 -0700

    [Studio] Modified screen "myapprovals"

--- client/controllers/app/myapprovals.js
+++ client/controllers/app/myapprovals.js
@@ -35,8 +35,7 @@ function app_myapprovals($scope, app, $ionicPopup){
    });
 
    alertPopup.then(function(res) {
-       $scope.data="";
-            console.log($scope.data+"-->Scopedata");
+      $scope.data.successmsg=undefined;
      console.log('Reload same approval page');
      var params={"employeeID":$localStorage.employeeID};
      app.call('goeasymethods.getMyApprovals',params);
