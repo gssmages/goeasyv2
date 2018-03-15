dca01da01e51894ce050e529e29849b86a67b0e4
Magesh Babu S <mageshbabu.s@flex.com>
2018-03-15 05:53:46 -0700

    [Studio] Modified screen "tripCancellation"

--- client/controllers/app/tripcancellation.js
+++ client/controllers/app/tripcancellation.js
@@ -2,13 +2,13 @@ angular.module('app').controller('app_tripcancellation', app_tripcancellation);
 function app_tripcancellation($scope, app,$filter) {
     'use strict';
     app.init($scope,function(){
-        $scope.fromdate="";
-        $scope.todate="";
         $scope.date = $filter('date')(new Date(), 'MM-dd-yyyy');
-        $scope.data.fromdate=$scope.data.tripinfo.FromDateOpnNoShow==null?$scope.date:$scope.data.tripinfo.FromDateOpnNoShow.split('T')[0];
-        $scope.data.todate=$scope.data.tripinfo.ToDateOpnNoShow==null?$scope.date:$scope.data.tripinfo.ToDateOpnNoShow.split('T')[0];
-        $scope.data.minDate=$scope.data.tripinfo.FromDateOpnNoShow==null?$scope.date:$scope.data.tripinfo.FromDateOpnNoShow.split('T')[0];
-        $scope.data.maxDate=$scope.data.tripinfo.ToDateOpnNoShow==null?$scope.date:$scope.data.tripinfo.ToDateOpnNoShow.split('T')[0];
+        var fromdate=$filter('date')($scope.data.tripinfo.FromDateOpnNoShow.split('T')[0],'MM-dd-yyyy');
+        var todate=$filter('date')($scope.data.tripinfo.ToDateOpnNoShow.split('T')[0],'MM-dd-yyyy');
+        $scope.data.fromdate=$scope.data.tripinfo.FromDateOpnNoShow==null?$scope.date:fromdate;
+        $scope.data.todate=$scope.data.tripinfo.ToDateOpnNoShow==null?$scope.date:todate;
+        $scope.data.minDate=$scope.data.tripinfo.FromDateOpnNoShow==null?$scope.date:fromdate;
+        $scope.data.maxDate=$scope.data.tripinfo.ToDateOpnNoShow==null?$scope.date:todate;
       /*  $scope.requestfor=[
             { RequestForID:"1",RequestForName:"Pickup and Drop"},
             { RequestForID:"2",RequestForName:"Pickup"},
