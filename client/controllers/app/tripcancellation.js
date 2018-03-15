70910ba6471f10d2703d491171e52d05e1d36556
Magesh Babu S <mageshbabu.s@flex.com>
2018-03-15 07:26:06 -0700

    [Studio] Modified screen "tripCancellation"

--- client/controllers/app/tripcancellation.js
+++ client/controllers/app/tripcancellation.js
@@ -9,7 +9,7 @@ function app_tripcancellation($scope, app,$filter) {
         $scope.data.fromdate=$scope.data.tripinfo.FromDateOpnNoShow==null?$scope.date:fromdate;
         $scope.data.todate=$scope.data.tripinfo.ToDateOpnNoShow==null?$scope.date:todate;
         $scope.data.minDate=$scope.data.tripinfo.FromDateOpnNoShow==null?$scope.dbdate:$scope.data.tripinfo.FromDateOpnNoShow.split('T')[0];
-        $scope.data.maxDate=$scope.data.tripinfo.ToDateOpnNoShow==null?$scope.dbdate:$scope.data.tripinfo.ToDateOpnNoShow.split('T')[0];
+        $scope.data.maxDate=$scope.data.tripinfo.ToDateOpnNoShow==null?$'':$scope.data.tripinfo.ToDateOpnNoShow.split('T')[0];
       /*  $scope.requestfor=[
             { RequestForID:"1",RequestForName:"Pickup and Drop"},
             { RequestForID:"2",RequestForName:"Pickup"},
