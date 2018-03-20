0e603569a4e3ee8d9780d5e6ec522e8c536198a3
Magesh Babu S <mageshbabu.s@flex.com>
2018-03-20 03:14:14 -0700

    [Studio] Modified screen "alertwin"

--- client/controllers/app/alertwin.js
+++ client/controllers/app/alertwin.js
@@ -1,5 +1,5 @@
 angular.module('app').controller('app_alertwin', app_alertwin);
-function app_alertwin($scope, app, $ionicPopup) {
+function app_alertwin($scope, app, $ionicPopup,$localStorage) {
     'use strict';
     app.init($scope,function(data){
     });
@@ -10,12 +10,39 @@ function app_alertwin($scope, app, $ionicPopup) {
 angular.forEach(cabreqid, function(value, key) {
   this.push(value);
 }, cabid);
-        
+        var employeeID=$localStorage.employeeID;
          var statusid=$scope.data.approvalinfo.status;
          var comment=$scope.data.comments;
-         sendtoconfirm={"cabRequestID":cabid,"status":statusid,"approver":"880781","remarks":comment};
+         if(statusid==2)
+         {
+             if($scope.data.comments)
+             {
+                  sendtoconfirm={"cabRequestID":cabid,"status":statusid,"approver":employeeID,"remarks":comment};
        console.log(sendtoconfirm);
           app.call("goeasymethods.approvalrequest",sendtoconfirm);
+             }
+             else
+             {
+                 errorMsg();
+             }
+       }
+         else
+         {
+             sendtoconfirm={"cabRequestID":cabid,"status":statusid,"approver":"880781","remarks":comment};
+       console.log(sendtoconfirm);
+          app.call("goeasymethods.approvalrequest",sendtoconfirm);
+             
+         }
       };
+       var errorMsg = function(){
+            var alertPopup = $ionicPopup.alert({
+                 title: 'Warning',
+                 template: 'Please enter comments'
+            });
+
+             alertPopup.then(function(res) {
+               // Custom functionality....
+             });
+        }
       
 }
