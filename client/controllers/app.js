90de776eba9b692b579993e5fcd1e2d5c46fd3c6
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-03-20 23:56:27 -0700

    [Studio] Modified screen "goeasyv2"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -2,7 +2,7 @@ angular.module('app').controller('app', app);
 function app($scope, app,$localStorage,$filter) {
     'use strict';
      app.init($scope,function(){
-         $scope.displayName = '';
+        $scope.displayName = '';
         $scope.businessTitle='';
        console.log("getting display name : "+$localStorage.displayName);
        // $scope.displayName = $localStorage.displayName;
