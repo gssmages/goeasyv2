174c0521d690d40b8487fb270952cd68fc2545a8
gssmages <32328556+gssmages@users.noreply.github.com>
2018-03-23 15:21:31 +0530

    url params change

--- server/api/goeasymethods.js
+++ server/api/goeasymethods.js
@@ -9,7 +9,7 @@ exports.getDashboard = function (page,params) {
     var xmlhttp = new XMLHttpRequest();
     //params are hard coded , need to make it dynamic.
     //var url = "http://gssnte811.asia.ad.flextronics.com:4042/api/DashBoardApi/GetDashboardDetails/?todaysdate="+params.todaysdate+"&location="+params.locationname+"&employeeID="+params.employeeID;
-    var url = urlConfig.urlData.Dashboard+params.todaysdate+"&location="+params.locationname+"&employeeID="+params.employeeID;
+    var url = urlConfig.urlData.Dashboard+"todaysdate="+params.todaysdate+"&location="+params.locationname+"&employeeID="+params.employeeID;
     var reqResponse = [];
     
     xmlhttp.onreadystatechange = function () {
@@ -31,7 +31,7 @@ exports.getMytrips = function (page,params) {
     var xmlhttp = new XMLHttpRequest();
     
     //var url = "http://gssnte811.asia.ad.flextronics.com:4042/api/MyTripsApi/GetMyTripsDetails?todaydate="+params.todaysdate+"&employeeID="+params.employeeID;
-    var url = urlConfig.urlData.MyTrips+params.todaysdate+"&employeeID="+params.employeeID;
+    var url = urlConfig.urlData.MyTrips+"todaydate="+params.todaysdate+"&employeeID="+params.employeeID;
     var reqResponse = [];
     
     xmlhttp.onreadystatechange = function () {
@@ -60,7 +60,7 @@ exports.getAdhocdetails = function (page, params) {
     var xmlhttp = new XMLHttpRequest();
     
     //var url = "http://gssnte811.asia.ad.flextronics.com:4042/api/AdhocCabRequestApi/ReadAdhocCabRequestValues/?employeeID="+params.employeeID+"&locationID="+params.locationID;
-    var url = urlConfig.urlData.GetAdhoc+params.employeeID+"&locationID="+params.locationID;
+    var url = urlConfig.urlData.GetAdhoc+"employeeID="+params.employeeID+"&locationID="+params.locationID;
     var reqResponse = [];
     
     xmlhttp.onreadystatechange = function () {
@@ -82,7 +82,7 @@ exports.getMyApprovals = function (page, params) {
     var xmlhttp = new XMLHttpRequest();
     
     //var url = "http://gssnte811.asia.ad.flextronics.com:4042/api/CabApprovalApi/ReadPendingRequests/?status=1&loggedUser="+params.employeeID; //880781
-    var url = urlConfig.urlData.MyAproval+params.employeeID;
+    var url = urlConfig.urlData.MyAproval+"status=1&loggedUser="+params.employeeID;
     var reqResponse = [];
     
     xmlhttp.onreadystatechange = function () {
@@ -104,7 +104,7 @@ exports.sendNoshow = function (page, params) {
     var xmlhttp = new XMLHttpRequest();
     
     //var url = "http://gssnte811.asia.ad.flextronics.com:4042/api/CancelTransportRequestApi/SaveCancelRequests?RequestTypeName="+params.RequestTypeName+"&RequestForName="+params.RequestForName+"&ShiftTimeID="+params.ShiftTimeID+"&CabRequestID="+params.CabRequestID+"&FromDateOpnNoShow="+params.FromDateOpnNoShow+"&ToDateOpnNoShow="+params.ToDateOpnNoShow+"&RequestTypeID="+params.RequestTypeID+"&RequestForID="+params.RequestForID+"&locationID="+params.locationID+"&employeeID="+params.employeeID+"&RequestedForName="+params.RequestedForName;
-    var url = urlConfig.urlData.NoShow+params.RequestTypeName+"&RequestForName="+params.RequestForName+"&ShiftTimeID="+params.ShiftTimeID+"&CabRequestID="+params.CabRequestID+"&FromDateOpnNoShow="+params.FromDateOpnNoShow+"&ToDateOpnNoShow="+params.ToDateOpnNoShow+"&RequestTypeID="+params.RequestTypeID+"&RequestForID="+params.RequestForID+"&locationID="+params.locationID+"&employeeID="+params.employeeID+"&RequestedForName="+params.RequestedForName;
+    var url = urlConfig.urlData.NoShow+"RequestTypeName="+params.RequestTypeName+"&RequestForName="+params.RequestForName+"&ShiftTimeID="+params.ShiftTimeID+"&CabRequestID="+params.CabRequestID+"&FromDateOpnNoShow="+params.FromDateOpnNoShow+"&ToDateOpnNoShow="+params.ToDateOpnNoShow+"&RequestTypeID="+params.RequestTypeID+"&RequestForID="+params.RequestForID+"&locationID="+params.locationID+"&employeeID="+params.employeeID+"&RequestedForName="+params.RequestedForName;
     var reqResponse = [];
     
     xmlhttp.onreadystatechange = function () {
@@ -146,7 +146,7 @@ exports.approvalrequest = function (page, params) {
     var xmlhttp = new XMLHttpRequest();
     
     //var url = "http://gssnte811.asia.ad.flextronics.com:4042/api/cabapprovalapi/ApprovePendingRequests?cabRequestID="+params.cabRequestID+"&remarks="+params.remarks+"&status="+params.status+"&approver="+params.approver;
-    var url = urlConfig.urlData.ApprovalReq+params.cabRequestID+"&remarks="+params.remarks+"&status="+params.status+"&approver="+params.approver;
+    var url = urlConfig.urlData.ApprovalReq+"cabRequestID="+params.cabRequestID+"&remarks="+params.remarks+"&status="+params.status+"&approver="+params.approver;
     var reqResponse = [];
     
     xmlhttp.onreadystatechange = function () {
@@ -168,7 +168,7 @@ exports.adhocRequest = function (page, params) {
     var xmlhttp = new XMLHttpRequest();
     
     //var url = "http://gssnte811.asia.ad.flextronics.com:4042/api/AdhocCabRequestApi/SaveCabOperationDetails?LocationID="+params.LocationID+"&EmployeeID="+params.EmployeeID+"&RequestTypeID="+params.RequestTypeID+"&RequestTypeName="+params.RequestTypeName+"&RequestForID="+params.RequestForID+"&RequestForName="+params.RequestForName+"&FromDate="+params.FromDate+"&Todate="+params.Todate+"&CommonDate="+params.CommonDate+"&UserTime="+params.UserTime+"&Shift="+params.Shift+"&ShiftTimeName="+params.ShiftTimeName+"&AreaID="+params.AreaID+"&AreaName="+params.AreaName+"&BoardingPointID="+params.BoardingPointID+"&BoardingPointName="+params.BoardingPointName+"&SpecialNeed="+params.SpecialNeed+"&SpecialNeedReason="+params.SpecialNeedReason+"&Reason="+params.Reason+"&StatusID="+params.StatusID+"&IsActive="+params.IsActive+"&EmployeeName="+params.EmployeeName+"&CreatedBy="+params.CreatedBy;
-    var url = urlConfig.urlData.AdhocReq+params.LocationID+"&EmployeeID="+params.EmployeeID+"&RequestTypeID="+params.RequestTypeID+"&RequestTypeName="+params.RequestTypeName+"&RequestForID="+params.RequestForID+"&RequestForName="+params.RequestForName+"&FromDate="+params.FromDate+"&Todate="+params.Todate+"&CommonDate="+params.CommonDate+"&UserTime="+params.UserTime+"&Shift="+params.Shift+"&ShiftTimeName="+params.ShiftTimeName+"&AreaID="+params.AreaID+"&AreaName="+params.AreaName+"&BoardingPointID="+params.BoardingPointID+"&BoardingPointName="+params.BoardingPointName+"&SpecialNeed="+params.SpecialNeed+"&SpecialNeedReason="+params.SpecialNeedReason+"&Reason="+params.Reason+"&StatusID="+params.StatusID+"&IsActive="+params.IsActive+"&EmployeeName="+params.EmployeeName+"&CreatedBy="+params.CreatedBy;
+    var url = urlConfig.urlData.AdhocReq+"LocationID="+params.LocationID+"&EmployeeID="+params.EmployeeID+"&RequestTypeID="+params.RequestTypeID+"&RequestTypeName="+params.RequestTypeName+"&RequestForID="+params.RequestForID+"&RequestForName="+params.RequestForName+"&FromDate="+params.FromDate+"&Todate="+params.Todate+"&CommonDate="+params.CommonDate+"&UserTime="+params.UserTime+"&Shift="+params.Shift+"&ShiftTimeName="+params.ShiftTimeName+"&AreaID="+params.AreaID+"&AreaName="+params.AreaName+"&BoardingPointID="+params.BoardingPointID+"&BoardingPointName="+params.BoardingPointName+"&SpecialNeed="+params.SpecialNeed+"&SpecialNeedReason="+params.SpecialNeedReason+"&Reason="+params.Reason+"&StatusID="+params.StatusID+"&IsActive="+params.IsActive+"&EmployeeName="+params.EmployeeName+"&CreatedBy="+params.CreatedBy;
     var reqResponse = [];
     
     xmlhttp.onreadystatechange = function () {
