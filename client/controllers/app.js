51e7d6b19fb5545e5bb3902764bd4c5244ca6f44
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-03-23 02:02:54 -0700

    [Studio] Modified screen "goeasyv2"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -2,9 +2,10 @@ angular.module('app').controller('app', app);
 function app($scope, app,$localStorage,$filter) {
     'use strict';
     
-        if (window.isPlatform && window.isPlatform.powWow() && window.isPlatform.iOS() && window.cordova && window.cordova.plugins) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+         if (window.isPlatform && window.isPlatform.powWow() && window.isPlatform.iOS() && window.cordova && window.cordova.plugins) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
         }
+ 
         
         if (window.StatusBar) {
             StatusBar.hide();
