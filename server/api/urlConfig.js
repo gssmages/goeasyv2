8bb1dcfc6495e2321de65e7608e25545115f5b4b
gssmages <32328556+gssmages@users.noreply.github.com>
2018-04-02 12:30:42 +0530

    Sit Url

--- server/api/urlConfig.js
+++ server/api/urlConfig.js
@@ -1,11 +1,21 @@
 
+// exports.urlData = {
+//     LoginReq : 'http://gssnt806.asia.ad.flextronics.com:85/api/login/CheckLoginDetailEncryption?',
+//     Dashboard: 'http://gssnt806.asia.ad.flextronics.com:85/api/DashBoardApi/GetDashboardDetails/?',
+//     MyTrips  : 'http://gssnt806.asia.ad.flextronics.com:85/api/MyTripsApi/GetMyTripsDetails?',
+//     GetAdhoc : 'http://gssnt806.asia.ad.flextronics.com:85/api/AdhocCabRequestApi/ReadAdhocCabRequestValues/?',
+//     MyAproval: 'http://gssnt806.asia.ad.flextronics.com:85/api/CabApprovalApi/ReadPendingRequests/?',
+//     NoShow   : 'http://gssnt806.asia.ad.flextronics.com:85/api/CancelTransportRequestApi/SaveCancelRequests?',
+//     ApprovalReq: 'http://gssnt806.asia.ad.flextronics.com:85/api/cabapprovalapi/ApprovePendingRequests?',
+//     AdhocReq :'http://gssnt806.asia.ad.flextronics.com:85/api/AdhocCabRequestApi/SaveCabOperationDetails?'
+// };
 exports.urlData = {
-    LoginReq : 'http://gssnt806.asia.ad.flextronics.com:85/api/login/CheckLoginDetailEncryption?',
-    Dashboard: 'http://gssnt806.asia.ad.flextronics.com:85/api/DashBoardApi/GetDashboardDetails/?',
-    MyTrips  : 'http://gssnt806.asia.ad.flextronics.com:85/api/MyTripsApi/GetMyTripsDetails?',
-    GetAdhoc : 'http://gssnt806.asia.ad.flextronics.com:85/api/AdhocCabRequestApi/ReadAdhocCabRequestValues/?',
-    MyAproval: 'http://gssnt806.asia.ad.flextronics.com:85/api/CabApprovalApi/ReadPendingRequests/?',
-    NoShow   : 'http://gssnt806.asia.ad.flextronics.com:85/api/CancelTransportRequestApi/SaveCancelRequests?',
-    ApprovalReq: 'http://gssnt806.asia.ad.flextronics.com:85/api/cabapprovalapi/ApprovePendingRequests?',
-    AdhocReq :'http://gssnt806.asia.ad.flextronics.com:85/api/AdhocCabRequestApi/SaveCabOperationDetails?'
-};
+    LoginReq : 'http://gssnte811.asia.ad.flextronics.com:4042/api/login/CheckLoginDetailEncryption?',
+    Dashboard: 'http://gssnte811.asia.ad.flextronics.com:4042/api/DashBoardApi/GetDashboardDetails/?',
+    MyTrips  : 'http://gssnte811.asia.ad.flextronics.com:4042/api/MyTripsApi/GetMyTripsDetails?',
+    GetAdhoc : 'http://gssnte811.asia.ad.flextronics.com:4042/api/AdhocCabRequestApi/ReadAdhocCabRequestValues/?',
+    MyAproval: 'http://gssnte811.asia.ad.flextronics.com:4042/api/CabApprovalApi/ReadPendingRequests/?',
+    NoShow   : 'http://gssnte811.asia.ad.flextronics.com:4042/api/CancelTransportRequestApi/SaveCancelRequests?',
+    ApprovalReq: 'http://gssnte811.asia.ad.flextronics.com:4042/api/cabapprovalapi/ApprovePendingRequests?',
+    AdhocReq :'http://gssnte811.asia.ad.flextronics.com:4042/api/AdhocCabRequestApi/SaveCabOperationDetails?'
+}
