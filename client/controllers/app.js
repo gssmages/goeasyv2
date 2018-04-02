dac0c0144fa1a80c3ba861707bd959e46c76a072
selvabharathi <selvabharathi.p@flex.com>
2018-04-02 03:59:08 -0700

    [Studio] Modified screen "goeasyv2"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -89,9 +89,9 @@ app.loginIsAction = false;
             
         }
     };
-}
-$scope.showPosition = function(position) {
+    $scope.showPosition = function(position) {
     // x.innerHTML = "Latitude: " + position.coords.latitude + 
     // "<br>Longitude: " + position.coords.longitude;
     console.log("Latitude: " + position.coords.latitude +"--Longitude: " + position.coords.longitude);
+}
 }
