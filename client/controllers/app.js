a1766ce8cf439dca530bb7f5a1e065e946073cfd
Magesh Babu S <mageshbabu.s@flex.com>
2018-04-19 05:11:05 -0700

    [Studio] Modified screen "goeasyv2"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -41,7 +41,7 @@ app.loginIsAction = false;
           console.log("calling mytrips in menu call");
         if(menuname=="mytrips")
         {
-            params={"employeeID":$localStorage.employeeID,"todaysdate":todaysdate};
+            params={"employeeID":"588310","todaysdate":todaysdate};
             console.log(params);
             app.call('goeasymethods.getMytrips',params);
         }
