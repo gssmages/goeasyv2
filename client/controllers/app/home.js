90e5eacb78006ba9c7f8fe9c87eb12f900e4483a
selvabharathi <selvabharathi.p@flex.com>
2018-05-03 01:03:22 -0700

    [Studio] Modified screen "Home"

--- client/controllers/app/home.js
+++ client/controllers/app/home.js
@@ -1,16 +1,18 @@
 angular.module('app').controller('app_home', app_home);
 function app_home($scope, app, $localStorage) {
     'use strict';
-    $scope.pickupDriverNumber ="";
-    $scope.dropDriverNumber ="";
+    var pickupDriverNumber ="";
+    var dropDriverNumber ="";
     app.init($scope,function(){
         
         //console.log('data in home page-->',$scope.data.home.PickupRequestDetail);
        $localStorage.displayName = $scope.data.home.EmployeeDetails.DisplayName;
        $localStorage.businessTitle = $scope.data.home.EmployeeDetails.BusinessTitle;
       
-      $scope.pickupDriverNumber = $scope.data.home.PickupRequestDetail.DriverContact;
-      $scope.dropDriverNumber =  $scope.data.home.DropRequestDetail.DriverContact;
+      pickupDriverNumber = $scope.data.home.PickupRequestDetail.DriverContact;
+      dropDriverNumber =  $scope.data.home.DropRequestDetail.DriverContact;
+      
+      document.getElementById('pickupcontact').attr("href","tel:"+pickupDriverNumber);
       // $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|tel|coui):/);
       
        $scope.pickupdropdiv = true;
