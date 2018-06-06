3d0b20794308d31c4ed0fb6911516f95c91caa5c
Magesh Babu S <mageshbabu.s@flex.com>
2018-06-06 02:32:07 -0700

    [Studio] Modified screen "feedback"

--- client/controllers/app/feedback.js
+++ client/controllers/app/feedback.js
@@ -1,5 +1,20 @@
 angular.module('app').controller('app_feedback', app_feedback);
 function app_feedback($scope, app) {
     'use strict';
-    app.init($scope);
+    app.init($scope,function(){
+        $scope.feedbackfor = [];
+         $scope.feedbackfor=[
+            { RequestForID:"0",RequestForName:"Pickup and Drop"},
+            { RequestForID:"1",RequestForName:"Pickup"},
+            { RequestForID:"2",RequestForName:"Drop"}
+            ];
+            
+             $scope.category = [];
+         $scope.category=[
+            { CategoryID:"0",CategoryName:"Pickup and Drop"},
+            { CategoryID:"1",CategoryName:"Pickup"},
+            { CategoryID:"2",CategoryName:"Drop"}
+            ];
+        
+    });
 }
