2874088dc32dd66d4ea4bbc4f50c501914ef70b9
selvabharathi <selvabharathi.p@flex.com>
2018-08-10 06:34:00 -0700

    [Studio] Modified screen "goeasyv2"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -65,7 +65,7 @@ app.loginIsAction = false;
         }
          else if(menuname=="feedback")
         {
-            params={"employeeID":$localStorage.employeeID};
+            params={"":$localStorage.locationID};
             console.log(params);
             app.call('goeasymethods.showFeedback',params);
         }
