44acb497a5125f72f5abd4b238c8af19ea955389
gssmages <32328556+gssmages@users.noreply.github.com>
2018-09-25 14:30:10 +0530

    feedback url added in prod

--- server/api/urlConfig.js
+++ server/api/urlConfig.js
@@ -7,7 +7,9 @@
      MyAproval: 'http://gssnt806.asia.ad.flextronics.com:85/api/CabApprovalApi/ReadPendingRequests/?',
      NoShow   : 'http://gssnt806.asia.ad.flextronics.com:85/api/CancelTransportRequestApi/SaveCancelRequests?',
      ApprovalReq: 'http://gssnt806.asia.ad.flextronics.com:85/api/cabapprovalapi/ApprovePendingRequests?',
-     AdhocReq :'http://gssnt806.asia.ad.flextronics.com:85/api/AdhocCabRequestApi/SaveCabOperationDetails?'
+     AdhocReq :'http://gssnt806.asia.ad.flextronics.com:85/api/AdhocCabRequestApi/SaveCabOperationDetails?',
+       GetFeedback :'http://gssnt806.asia.ad.flextronics.com:85/api/FeedbackApi/GetMobileQuestions?',
+    SaveFeedback :'http://gssnt806.asia.ad.flextronics.com:85/api/FeedbackApi/SaveMobileFeedback?'
  };*/
 exports.urlData = {
     LoginReq : 'http://gssnte811.asia.ad.flextronics.com:4042/api/login/CheckLoginDetailEncryption?',
@@ -15,7 +17,7 @@ exports.urlData = {
     MyTrips  : 'http://gssnte811.asia.ad.flextronics.com:4042/api/MyTripsApi/GetMyTripsDetails?',
     GetAdhoc : 'http://gssnte811.asia.ad.flextronics.com:4042/api/AdhocCabRequestApi/ReadAdhocCabRequestValues/?',
     MyAproval: 'http://gssnte811.asia.ad.flextronics.com:4042/api/CabApprovalApi/ReadPendingRequests/?',
-    NoShow   : 'http://gssnte811.asia.ad.flextronics.com:4042/api/CancelTransportRequestApi/SaveCancelRequests?',
+    NoShow: 'http://gssnte811.asia.ad.flextronics.com:4042/api/CancelTransportRequestApi/SaveCancelRequests?',
     ApprovalReq: 'http://gssnte811.asia.ad.flextronics.com:4042/api/cabapprovalapi/ApprovePendingRequests?',
     AdhocReq :'http://gssnte811.asia.ad.flextronics.com:4042/api/AdhocCabRequestApi/SaveCabOperationDetails?',
     GetFeedback :'http://gssnte811.asia.ad.flextronics.com:4042/api/FeedbackApi/GetMobileQuestions?',
