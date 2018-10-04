81a0deedc374b2d936860749cd9a060d0087a3ea
Magesh Babu S <mageshbabu.s@flex.com>
2018-10-04 05:09:29 -0700

    [Studio] Modified screen "specialcabapproval"

--- client/controllers/app/specialcabapproval.js
+++ client/controllers/app/specialcabapproval.js
@@ -1,4 +1,33 @@
-angular.module('app').controller('app_specialcabapproval', app_specialcabapproval);
+angular.module('app').controller('app_specialcabapproval', app_specialcabapproval)
+.directive("approvallist",function($ionicGesture,$rootScope){
+    return {
+        scope: {},
+        restrict: "A",
+        link: function(scope, elem, attrs) {
+           // elem.css('border-top','5px solid #ccc');  
+            // onHold => start select box by `onHold` => working good
+          $ionicGesture.on('hold', function() {
+            elem.addClass("selected").css("background","#fff");
+            elem[0].querySelector('.tickmark').style.display="block";
+            elem[0].querySelector('.calendericon').style.display="none";
+            $rootScope.startSelect = true; // to enable select box by click
+          }, elem);
+		$ionicGesture.on('tap',function(){
+        if ($rootScope.startSelect) {
+           if (elem.hasClass('selected')) {
+               elem.removeClass('selected').css("background","#fff");
+                elem[0].querySelector('.tickmark').style.display="none";
+                elem[0].querySelector('.calendericon').style.display="block";
+              } else {
+               elem.addClass('selected').css("background","#fff");
+                elem[0].querySelector('.tickmark').style.display="block";
+                elem[0].querySelector('.calendericon').style.display="none";
+              }
+            }
+          }, elem);
+        }
+    };
+});
 function app_specialcabapproval($scope, app) {
     'use strict';
     app.init($scope);
