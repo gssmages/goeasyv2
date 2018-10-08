fda482e269ef5e51f79703fa299196b337e57979
Magesh Babu S <mageshbabu.s@flex.com>
2018-10-07 23:52:35 -0700

    [Studio] Modified screen "specialcabcomments"

--- client/controllers/app/specialcabcomments.js
+++ client/controllers/app/specialcabcomments.js
@@ -1,5 +1,48 @@
 angular.module('app').controller('app_specialcabcomments', app_specialcabcomments);
 function app_specialcabcomments($scope, app) {
     'use strict';
-    app.init($scope);
+    app.init($scope, function (data) {
+    });
+    $scope.sendrequest = function () {
+        var sendtoconfirm = '';
+        var cabreqid = $scope.data.approvalinfo.cabRequestID;
+        var cabid = [];
+        angular.forEach(cabreqid, function (value, key) {
+            this.push(value);
+        }, cabid);
+        var employeeID = $localStorage.employeeID;
+        var statusid = $scope.data.approvalinfo.status;
+        var comment = $scope.data.comments;
+        if (statusid == 3) {
+            if ($scope.data.comments) {
+                sendtoconfirm = {
+                    'cabRequestID': cabid,
+                    'status': statusid,
+                    'approver': employeeID,
+                    'remarks': comment
+                };
+                console.log(sendtoconfirm);
+                app.call('goeasymethods.approvalrequest', sendtoconfirm);
+            } else {
+                errorMsg();
+            }
+        } else {
+            sendtoconfirm = {
+                'cabRequestID': cabid,
+                'status': statusid,
+                'approver': employeeID,
+                'remarks': comment
+            };
+            console.log(sendtoconfirm);
+            app.call('goeasymethods.approvalrequest', sendtoconfirm);
+        }
+    };
+    var errorMsg = function () {
+        var alertPopup = $ionicPopup.alert({
+            title: 'Warning',
+            template: 'Please enter comments'
+        });
+        alertPopup.then(function (res) {
+        });
+    };
 }
