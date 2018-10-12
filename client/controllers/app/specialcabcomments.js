cae4908cf2c20e2fb1bce478339d6c6c1e023667
Magesh Babu S <mageshbabu.s@flex.com>
2018-10-12 04:09:18 -0700

    [Studio] Modified screen "specialcabcomments"

--- client/controllers/app/specialcabcomments.js
+++ client/controllers/app/specialcabcomments.js
@@ -20,7 +20,7 @@ function app_specialcabcomments($scope, app,$ionicPopup, $localStorage) {
         }, cabid);
         var employeeID = $localStorage.employeeID;
         var statusid = $scope.data.approvalinfo.status;
-        var comment = $scope.data.comments;
+        var comment = $scope.data.comments==undefined?"":$scope.data.comments;
         if (statusid == 3) {
             if ($scope.data.comments) {
                 sendtoconfirm = {
