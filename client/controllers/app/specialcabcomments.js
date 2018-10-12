ee8e590c744bbe53375d78ee10fd8be6e2d3529e
Magesh Babu S <mageshbabu.s@flex.com>
2018-10-12 06:42:15 -0700

    [Studio] Modified screen "specialcabcomments", Modified screen "alertwin"

--- client/controllers/app/specialcabcomments.js
+++ client/controllers/app/specialcabcomments.js
@@ -8,7 +8,7 @@ function app_specialcabcomments($scope, app,$ionicPopup, $localStorage) {
      }
      else
      {
-         $scope.data.commentlabel="Enter Reject Comments <span style='color:red'>*</span>";
+         $scope.data.commentlabel="Enter Reject Comments <span style=\"color:red\">*</span>";
      }
     });
     $scope.sendrequest = function () {
